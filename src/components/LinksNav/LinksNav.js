17a0b061a88a5e8c2ae4428c3be82702d2b25fd5
Kersul <joao.faria@creditas.com>
2021-08-21 23:56:19 -0300

    feat: new colors ui

--- src/components/LinksNav/LinksNav.js
+++ src/components/LinksNav/LinksNav.js
@@ -7,10 +7,10 @@ import colors from '../../config/colors';
 
 const LinksNav = () => (
   <Box direction="row" justify="center" background={colors.alternateBackground} width="full">
-    <Button icon={<Github color={colors.primary} />} hoverIndicator href="https://github.com/jhkersul" target="_blank" />
-    <Button icon={<Linkedin color={colors.primary} />} hoverIndicator href="https://www.linkedin.com/in/kersul/" target="_blank" />
-    <Button icon={<Instagram color={colors.primary} />} hoverIndicator href="https://www.instagram.com/jhkersul/" target="_blank" />
-    <Button icon={<Twitter color={colors.primary} />} hoverIndicator href="https://twitter.com/JHKersul" target="_blank" />
+    <Button icon={<Github color={colors.secondary} />} hoverIndicator href="https://github.com/jhkersul" target="_blank" />
+    <Button icon={<Linkedin color={colors.secondary} />} hoverIndicator href="https://www.linkedin.com/in/kersul/" target="_blank" />
+    <Button icon={<Instagram color={colors.secondary} />} hoverIndicator href="https://www.instagram.com/jhkersul/" target="_blank" />
+    <Button icon={<Twitter color={colors.secondary} />} hoverIndicator href="https://twitter.com/JHKersul" target="_blank" />
   </Box>
 )
 
--- src/config/colors.js
+++ src/config/colors.js
@@ -1,9 +1,11 @@
 const colors = {
-  primary: '#66FCF1',
-  secondary: '#45A29F',
+  // primary: '#66FCF1',
+  secondary: '#93a3f1',
+  // secondary: '#4f86ff',
+  primary: '#4f86ff',
   terciary: '#C4C6C8',
   alternateBackground: '#0B0D10',
-  background: '#202832',
+  background: '#2e5bbc',
 }
 
 export default colors
